14f7c7576c8ea2ec28b744de9de0f7355957686d
Felix <dede4861@gmail.com>
2022-06-13 15:03:53 +0200

    QRFact working

--- Uebung2/QRFact.m
+++ Uebung2/QRFact.m
@@ -4,44 +4,40 @@ function [A, D, pi , k ] = QRFact (A)
 
 
 pi = 1:n; %p=pi
-si = zeros(n);
-siq = zeros(n);
+si = zeros(n,1);
+%siq = zeros(n,1);
 
 D = zeros(min(m,n),1);
 
 nq = n;
 
 for j = n:-1:1 
-    for i = 1:m
-        siq(j) = dot(A(:,j),A(:,j));%siq(j) + A(i,j)^2;
-    end
-    si(j) = siq(j);
+    si(j) = dot(A(:,j),A(:,j));
     if si(j) == 0
-        temp1 = pi(j);
+        temp1 = pi(j); %oder [pi(j),pi(nq)] = deal(pi(nb),pi(j));
         pi(j) = pi(nq);
         pi(nq) = temp1;
         nq = nq-1;
     end
 end
+siq = si;
+
+%drüber is ok
 
 for i = 1:nq%different loop iterator than in script, here i is k
-    piv = i-1;
-    val = -1;
 
     [val,piv] = max(si(pi(i:nq))./siq(pi(i:nq)));
     
-    if piv < i
+    if val <= -1
         k =i-1;
         return;
     end
+
     temp1 = pi(i);
     pi(i) = pi(piv);
     pi(piv) = temp1;
-    
-    for j = i:m
-        si(pi(i)) = 0;%maybe not necessarry
-        si(pi(i)) = si(pi(i)) + A(j,pi(i));
-    end
+
+    si(pi(i)) = dot(A(:,pi(i)),A(:,pi(i)));
 
     if si(pi(i)) < m * eps^2 * siq(pi(i))
         k = i-1;
@@ -57,14 +53,11 @@ for i = 1:nq%different loop iterator than in script, here i is k
     A(i,pi(i)) = A(i, pi(i)) - D(i,1);
 
     for j = i+1:nq
-        gamma = dot(A(i,pi(j)),A(i,pi(i))) / (-D(i,1)*A(i,pi(i))); %dot() is scalarproduct
-        A(i:n, pi(j)) = A(i:n, pi(j)) - gamma * A(i:n, pi(j));
+        gamma = dot(A(i:m,pi(j)),A(i:m,pi(i))) / (-D(i,1)*A(i,pi(i))); %dot() is scalarproduct
+        A(i:m, pi(j)) = A(i:m, pi(j)) - gamma * A(i:m, pi(i));
         si(pi(j)) = si(pi(j)) - A(i, pi(j))^2;
         if si(pi(j)) < m * eps * siq(pi(j))
-            for l = i+1 : m
-            si(pi(j)) = 0;
-            si(pi(j)) = si(pi(j)) + A(l,pi(j))^2;
-            end
+            si(pi(j)) = dot(A(l,pi(j)),A(l,pi(j)));
         end
     end
 end
