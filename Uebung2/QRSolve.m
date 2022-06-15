61c53bf6b20b91c9d7437447b0723400a3fd6b57
Felix <dede4861@gmail.com>
2022-06-15 12:29:13 +0200

    editing for readabillity

--- Uebung2/QRSolve.m
+++ Uebung2/QRSolve.m
@@ -1,39 +1,25 @@
 function [ x ] = QRSolve (B,D, p , k , b )
 
-%[A,d,p,k] = QRFact(B);
-[m,n] = size(B);
+[~,n] = size(B);
 
 if k < n
     x = zeros(n:1);
 else
-    %c = b ;
-    %for j =1: k
-    %    v= zeros (m ,1) ;
-    %    if(j >1)
-    %    v (1: j -1) =0;
-    %    end
-    %    v(j :m)=B (j:m ,p(j) );
-
-    %    c= eye (m ) -(2/ dot(v ,v)) * dot(v,v)* c;
-    %end
 
     Q = CompQ(B,p,k);
     Qt = transpose(Q);
     c= Qt * b';
 
-    R = CompR(B,D,p,k);    
-    %Rt = transpose(R);
-    %x = Rt * c;
-    R=B (: , p) ;
-    x= zeros (n ,1) ;
+    R=B (:,p) ;
+    x= zeros (n,1) ;
 
-    pi (p) =1: length (p );
+    pi(p) = 1:length(p);
 
-    x(k )=c(k )/D( k);
+    x(k) = c(k)/D(k);
     for i=k -1: -1:1
-        x(i ) =( c( i) -dot (R(i ,i +1: k) ,x(i +1: k))) /D(i );
+        x(i) =(c(i)-dot(R(i,i+1:k) , x(i+1:k))) / D(i);
     end
-    x=x ( pi );
+    x = x(pi);
 
 end
 end
