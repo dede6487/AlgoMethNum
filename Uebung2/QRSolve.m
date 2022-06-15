a105a869d57c6442fe71d4236eccb1dccaa58a62
Felix <dede4861@gmail.com>
2022-06-15 10:14:18 +0200

    trying to fix QR solve

--- Uebung2/QRSolve.m
+++ Uebung2/QRSolve.m
@@ -6,15 +6,34 @@ function [ x ] = QRSolve (B,D, p , k , b )
 if k < n
     x = zeros(n:1);
 else
+    c = b ;
+    for j =1: k
+        v= zeros (m ,1) ;
+        if(j >1)
+        v (1: j -1) =0;
+        end
+        v(j :m)=B (j:m ,p(j) );
 
-    Q = CompQ(B,p,k);
-    R = CompR(B,D,p,k);
+        c= eye (m ) -(2/ dot(v ,v)) * dot(v,v)* c;
+    end
 
-    Qt = transpose(Q);
-    Rt = transpose(R);
-    
-    c= Qt * b';
+    %Q = CompQ(B,p,k);
+    %Qt = transpose(Q);
+    %c= Qt * b';
 
-    x = Rt * c;
+    %R = CompR(B,D,p,k);    
+    %Rt = transpose(R);
+    %x = Rt * c;
+    R=B (: , p) ;
+    x= zeros (n ,1) ;
+
+    pi (p) =1: length (p );
+
+    x(k )=c(k )/D( k);
+    for i=k -1: -1:1
+        x(i ) =( c( i) -dot (R(i ,i +1: k) ,x(i +1: k))) /D(i );
+    end
+    x=x ( pi );
+
 end
 end
