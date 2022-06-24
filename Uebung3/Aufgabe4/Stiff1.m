4d8389ebdf64df37b1484211c7e97a3110c99f74
Felix <dede4861@gmail.com>
2022-06-24 12:02:26 +0200

    working on stiff1/2

--- Uebung3/Aufgabe4/Stiff1.m
+++ Uebung3/Aufgabe4/Stiff1.m
@@ -1,7 +1,7 @@
 function [K_h] = Stiff1(N)
 
-i = N-1;
-j = i;
+m = (N-1)^2;
+n = m;
 
 K_h = sparse(i,j,s,m,n);
 
--- Uebung3/Aufgabe4/Stiff2.m
+++ Uebung3/Aufgabe4/Stiff2.m
@@ -1,7 +1,11 @@
 function [K_h] = Stiff2(N)
 
+n = (N-1)^2;
 
+e = ones(n,1);
 
-K_h = spdiags(B,d,m,n);
+K_h = spdiags([-e 4*e -e],-1:1,n,n);
+
+%K_h = K_h + spdiags([-e -e],-N+1:N-1,n,n);
 
 end
